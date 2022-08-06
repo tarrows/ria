f241b994f16c3c0ecfb8f0bfa6e68fede3fcaa53
tarrows <rem_tate@yahoo.co.jp>
2022-08-07 00:45:56 +0900

    read file

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let args = App::new("grep-lite")
@@ -11,16 +14,24 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
+
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, 
-    and dark square is a picture feverishly turned--in search of what?
-    It is the same with books. What do we seek throught millions of pages?";
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        match re.find(line) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
